e6544fa484c0d0fd31911122ec5dea83c859feec
Ivanyinfan <1315328976@qq.com>
2017-11-24 10:33:42 -0500

    [FINISH] F_newFrame;F_name;F_formals;F_allocLocal

--- frame.h
+++ frame.h
@@ -13,6 +13,11 @@ typedef struct F_accessList_ *F_accessList;
 
 struct F_accessList_ {F_access head; F_accessList tail;};
 
+F_frame F_newFrame(Temp_label name, U_boolList formals);
+
+Temp_label F_name(F_frame f);
+F_accessList F_formals(F_frame f);
+F_access F_allocLocal(F_frame f, bool escape);
 
 /* declaration for fragments */
 typedef struct F_frag_ *F_frag;
--- x86frame.c
+++ x86frame.c
@@ -11,6 +11,12 @@
 
 /*Lab5: Your implementation here.*/
 
+struct F_frame_ {
+    Temp_label name;
+    F_accessList formals;
+    int localNum;
+};
+
 //varibales
 struct F_access_ {
 	enum {inFrame, inReg} kind;
@@ -20,15 +26,59 @@ struct F_access_ {
 	} u;
 };
 
+F_accessList F_AccessList(F_access head, F_accessList tail)
+{
+	F_accessList al=checked_malloc(sizeof(*al));
+    al->head = head;
+    al->tail = tail;
+    return al;
+}
+
+F_frame F_newFrame(Temp_label name, U_boolList formals)
+{
+	F_frame f=checked_malloc(sizeof(*f));
+	f->name=name;
+	f->formals=F_AccessList2(formals);
+	localNum=0;
+}
+
+Temp_label F_name(F_frame f)
+{
+	return f->name;
+}
+
+F_accessList F_formals(F_frame f)
+{
+	return f->formals;
+}
+
+F_access F_allocLocal(F_frame f, bool escape)
+{
+	if(!escape)
+		return InReg(Temp_newtemp());
+	++f->localNum;
+	return InFrame(-F_worldSize*f->localNum);
+}
+
 F_frag F_StringFrag(Temp_label label, string str) {   
-	    return NULL;                                      
+	F_frag f=checked_malloc(sizeof(*f));
+	f->kind=F_stringFrag;
+	f->u.stringg.label=label;
+	f->u.stringg.str=str;
+	return f;                                    
 }                                                     
                                                       
 F_frag F_ProcFrag(T_stm body, F_frame frame) {        
-	    return NULL;                                      
+	F_frag f=checked_malloc(sizeof(*f));
+	f->kind=F_procFrag;
+	f->u.proc.body=body;
+	f->u.proc.frame=frame;
+	return f;                                
 }                                                     
                                                       
 F_fragList F_FragList(F_frag head, F_fragList tail) { 
-	    return NULL;                                      
-}                                                     
-
+	F_fragList f=checked_malloc(sizeof(*f));
+	f->head=head;
+	f->tail=tail;
+	return f;
+}
